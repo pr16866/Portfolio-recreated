c85a091904f592322ad2813d8d3d2e9f95fabb27
Piyush Priyam <pr16866@gmail.com>
2023-04-13 10:15:33 +0530

    first commit

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useRef } from "react";
+import style from "./home.module.css";
+import * as THREE from "three";
+import moonImage from "../../Images/moon.jpg";
+import venusImage from "../../Images/venus.jpg";
+import spaceImage from "../../Images/space.jpg";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Typography } from "@mui/material";
+import Timelines from "../Timeline";
+// import { Timeline } from "@mui/icons-material";
+// import TimeLine from "../TimeLine/TimeLine";
+function Home() {
+  const canvasRef = useRef(null);
+  const { homeCanvas } = style;
+
+  useEffect(() => {
+    const textureLoader = new THREE.TextureLoader();
+
+    const moonTexture = textureLoader.load(moonImage);
+    const venusTexture = textureLoader.load(venusImage);
+    const spaceTexture = textureLoader.load(spaceImage);
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(4, 4, 8);
+
+    //  const canvas = canvasRef;
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+
+    const moonGeometry = new THREE.SphereGeometry(2, 64, 64);
+    const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture });
+    const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+
+    const venusGeometry = new THREE.SphereGeometry(3, 64, 64);
+    const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
+    const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+    venus.position.set(8, 5, 5);
+
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    const pointLight2 = new THREE.PointLight(0xffffff, 0.1);
+
+    pointLight.position.set(8, 5, 5);
+    pointLight2.position.set(-8, -5, -5);
+
+    scene.add(moon);
+    scene.add(venus);
+    scene.add(pointLight);
+    scene.add(pointLight2);
+    scene.background = spaceTexture;
+
+    const constSpeed = 0.01;
+    window.addEventListener("mousemove", (e) => {
+      if (e.clientX <= window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
+
+      if (e.clientX > window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+
+      if (e.clientY > window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
+
+      if (e.clientY <= window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      moon.rotation.y += 0.001;
+      venus.rotation.y += 0.001;
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return window.addEventListener("scroll", () => {
+      camera.rotation.z = window.scrollY * 0.001;
+      camera.rotation.y = window.scrollY * 0.003;
+
+      const skillsBox = document.getElementById("homeskillsBox");
+
+      if (window.scrollY > 1500) {
+        skillsBox.style.animationName = "homeskillsBoxAnimationOn";
+      } else {
+        skillsBox.style.animationName = "homeskillsBoxAnimationOff";
+      }
+    });
+  }, []);
+
+  return (
+    <div className="home">
+      <canvas className={homeCanvas} ref={canvasRef}>
+        <div className="homeContainer">
+          <Typography variant="h3">TIMELINE</Typography>
+          <Timelines timelines={[1, 2, 3, 4]} />
+        </div>
+      </canvas>
+    </div>
+  );
+}
+
+export default Home;
